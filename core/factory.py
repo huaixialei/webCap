f5f9ddcd2d01ddc63abe23c3ba36913bc62cde30
xialei <blogdown@126.com>
2018-06-26 23:18:19 +0800

    update

--- core/factory.py
+++ core/factory.py
@@ -1,5 +1,4 @@
 from tcpdump import Tcpdump
-import sys
 
 tcpdump_map = {}
 
@@ -14,16 +13,3 @@ def kill_tcpdump(key):
     if tcpdump_map.has_key(str):
         tcpdump_map[key].kill()
     return
-
-
-# &&(tcp[(tcp[12:1]>>2):2] == 0x4745)
-t = generate_tcpdump("tcpdump -i en4 -w t.pcap '(tcp)&&(src host 10.10.32.35)'", 'shut\n')
-t.capture()
-
-while True:
-    str = sys.stdin.readline()
-    if tcpdump_map.has_key(str):
-        kill_tcpdump(str)
-        break
-
-# t()
